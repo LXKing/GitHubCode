4f0fb483fe513e9a6de3ecfba0f214d837df1687
wxytkzc <291026174@qq.com>
2018-06-08 09:09:02 +0800

    提交

--- Exam/Code/ExamClient/ExamClientWeb/Js/Default.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="../Scripts/jquery-2.1.4.min.js" />
-/// <reference path="../Scripts/jqwidgets/jqx-all.js" />
-/// <reference path="../Scripts/jqwidgets/jqxtabs.js" />
-
--- Exam/Code/ExamOnLine/ueditor/net/controller.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" CodeBehind="controller.ashx.cs" Class="ExamOnLine.ueditor.net.UEditorHandler" %>
